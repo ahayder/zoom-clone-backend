cac80b695e4b3a79208f5ebce0c1cfb2ad995181
N. M. Ali Hayder <ahayder@loobylab.com>
2020-11-03 18:57:32 +0600

    import to require added

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const http = require('http');
 const app = express();
 const server = http.createServer(app);
 const io = require('socket.io')(server);
-import cors from 'cors';
+const cors = require('cors');
 
 app.use(cors())
 
